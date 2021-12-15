1be1a84c264d88b0ce0cb143be95faefb421f740
Sterling Greene <sterling@gradle.com>
2021-12-15 16:21:15 -0500

    WIP hacky derived variants

---
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/derived/DerivedVariantsResolutionIntegrationTest.groovy
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.resolve.derived
+
+
+import org.gradle.integtests.fixtures.AbstractHttpDependencyResolutionTest
+import org.gradle.test.fixtures.server.http.MavenHttpModule
+
+class DerivedVariantsResolutionIntegrationTest extends AbstractHttpDependencyResolutionTest {
+    MavenHttpModule direct
+    MavenHttpModule transitive
+
+    def setup() {
+        buildFile << """
+            plugins {
+                id 'java'
+            }
+
+            repositories {
+                maven { url '$mavenHttpRepo.uri' }
+            }
+            
+            dependencies {
+                implementation 'test:direct:1.0'
+            }
+            
+            abstract class Resolve extends DefaultTask {
+                @InputFiles
+                abstract ConfigurableFileCollection getArtifacts()
+                
+                @Internal
+                List<String> expectations = []
+                
+                @TaskAction
+                void assertThat() {
+                    assert artifacts.files*.name == expectations 
+                }
+            }
+            
+            task resolve(type: Resolve) {
+                def artifactView = configurations.runtimeClasspath.incoming.artifactView {
+                    lenient = true
+                    
+                    attributes {
+                        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.JAVA_RUNTIME))
+                        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
+                        attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling, Bundling.EXTERNAL))
+                        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.SOURCES))
+                    }
+                }
+                artifacts.from(artifactView.getFiles())
+            }
+        """
+        transitive = mavenHttpRepo.module("test", "transitive", "1.0")
+        direct = mavenHttpRepo.module("test", "direct", "1.0")
+        direct.dependsOn(transitive)
+
+        //         transitive.withSourceAndJavadoc()
+        //         transitive.withModuleMetadata()
+    }
+
+    def "direct has no GMM and no sources jar"() {
+        transitive.publish()
+        direct.publish()
+
+        buildFile << """
+            resolve {
+                expectations = []
+            }
+        """
+        expect:
+        direct.pom.expectGet()
+        transitive.pom.expectGet()
+        direct.artifact(classifier: "sources").expectGetMissing()
+        transitive.artifact(classifier: "sources").expectGetMissing()
+
+        succeeds( "resolve")
+    }
+
+    def "direct has GMM and has sources jar"() {
+        transitive.adhocVariants().variant("sources", [
+                "org.gradle.category": "documentation",
+                "org.gradle.dependency.bundling": "external",
+                "org.gradle.docstype": "sources",
+                "org.gradle.usage": "java-runtime"
+        ]) {
+            artifact("transitive-1.0-sources.jar")
+        }
+        transitive.withModuleMetadata()
+        transitive.publish()
+
+        direct.adhocVariants().variant("sources", [
+                "org.gradle.category": "documentation",
+                "org.gradle.dependency.bundling": "external",
+                "org.gradle.docstype": "sources",
+                "org.gradle.usage": "java-runtime"
+        ]) {
+            artifact("direct-1.0-sources.jar")
+        }
+        direct.withModuleMetadata()
+        direct.publish()
+
+        buildFile << """
+            resolve {
+                expectations = ['direct-1.0-sources.jar', 'transitive-1.0-sources.jar']
+            }
+        """
+        expect:
+        direct.pom.expectGet()
+        direct.moduleMetadata.expectGet()
+        transitive.pom.expectGet()
+        transitive.moduleMetadata.expectGet()
+        direct.artifact(classifier: "sources").expectGet()
+        transitive.artifact(classifier: "sources").expectGet()
+
+        succeeds( "resolve")
+    }
+//
+//    def "direct has no GMM and has sources jar"() {
+//        direct.withSourceAndJavadoc()
+//        transitive.withSourceAndJavadoc()
+//
+//        transitive.publish()
+//        direct.publish()
+//
+//        buildFile << """
+//            resolve {
+//                expectations = ["direct-1.0-sources.jar", "transitive-1.0-sources.jar"]
+//            }
+//        """
+//        expect:
+//        succeeds("resolve")
+//    }
+}
--- subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/model/JavaEcosystemVariantDerivationStrategy.java
+++ subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/model/JavaEcosystemVariantDerivationStrategy.java
@@ -60,12 +60,17 @@ public class JavaEcosystemVariantDerivationStrategy extends AbstractStatelessDer
                     // component we cannot mix precise usages with more generic ones)
                 libraryWithUsageAttribute(compileConfiguration, attributes, attributesFactory, Usage.JAVA_API),
                 libraryWithUsageAttribute(runtimeConfiguration, attributes, attributesFactory, Usage.JAVA_RUNTIME),
+                    //sourcesVariant(attributesFactory, attributes, runtimeConfiguration),
                 platformWithUsageAttribute(compileConfiguration, attributes, attributesFactory, Usage.JAVA_API, false, shadowedPlatformCapability),
                 platformWithUsageAttribute(runtimeConfiguration, attributes, attributesFactory, Usage.JAVA_RUNTIME, false, shadowedPlatformCapability),
                 platformWithUsageAttribute(compileConfiguration, attributes, attributesFactory, Usage.JAVA_API, true, shadowedEnforcedPlatformCapability),
                 platformWithUsageAttribute(runtimeConfiguration, attributes, attributesFactory, Usage.JAVA_RUNTIME, true, shadowedEnforcedPlatformCapability));
         }
         return null;
+    }
+
+    private DefaultConfigurationMetadata sourcesVariant(MavenImmutableAttributesFactory attributesFactory, ImmutableAttributes originAttributes, DefaultConfigurationMetadata runtimeConfiguration) {
+        return runtimeConfiguration.mutate().withAttributes(attributesFactory.sourcesVariant(originAttributes)).withoutConstraints().build();
     }
 
     private ImmutableCapabilities buildShadowPlatformCapability(ModuleComponentIdentifier componentId, boolean enforced) {
